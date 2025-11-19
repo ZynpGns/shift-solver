fd9ea42644e18bf2f43ee874a4c744ec386f20d7
ZynpGns <49254826+ZynpGns@users.noreply.github.com>
2025-11-19 23:47:50 +0300

    Update Rules.java

--- src/main/java/org/acme/employeescheduling/domain/Rules.java
+++ src/main/java/org/acme/employeescheduling/domain/Rules.java
@@ -4,6 +4,9 @@ public class Rules {
 
     // dakika cinsinden; örn: 55 saat = 3300 dk
     private long maxWeeklyMinutes = 3300;
+    
+    //Günlük maximum 11 saat çalışma sınırı
+    private long dailyMaxMinutes = 11 * 60;
 
     public Rules() {}
 
@@ -18,4 +21,7 @@ public class Rules {
     public void setMaxWeeklyMinutes(long maxWeeklyMinutes) {
         this.maxWeeklyMinutes = maxWeeklyMinutes;
     }
+
+    public long getDailyMaxMinutes() { return dailyMaxMinutes; }
+    public void setDailyMaxMinutes(long v) { this.dailyMaxMinutes = v; }
 }
